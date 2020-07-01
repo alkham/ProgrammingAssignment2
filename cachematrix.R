36a961a158de3924f0a3bcfd4cd985ee850cdf7f
Alkham <48203102+alkham@users.noreply.github.com>
2020-07-01 19:45:16 +0600

    My code for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function makes special vector to store cache inverse.
+## The second one, firstly checks the existence of cached inverse matrix and if there is no such matrix, it calculates it.
 
-## Write a short comment describing this function
+## Makes a list for a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      ins <- NULL
+      setMatrix <- function(y) {
+        x <<- y
+        ins <<- NULL
+      }
+      
+      getMatrix <- function() {
+        x
+      }
+      
+      setInverse <- function(inverse) {
+        ins <<- inverse
+      }
+      getInverse <- function() {
+        ins
+      }
+      
+      list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  
+      ins <- x$getInverse()
+      
+      if(!is.null(ins)) {
+        message("getting cashed data")
+        return(ins)
+      }
+      
+      data <- x$getMatrix()
+      
+      ins <- solve(data, ...)
+      x$setInverse(ins)
+      ins
+      
 }
